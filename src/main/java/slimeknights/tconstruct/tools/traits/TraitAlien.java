596d6f1815a940b2cc88361c991400a72f23aef1
LongLoser0 <89991671+LongLoser0@users.noreply.github.com>
2021-09-02 15:34:51 -0300

    Update TraitAlien.java

--- src/main/java/slimeknights/tconstruct/tools/traits/TraitAlien.java
+++ src/main/java/slimeknights/tconstruct/tools/traits/TraitAlien.java
@@ -95,7 +95,7 @@ public class TraitAlien extends TraitProgressiveStats {
       // ok, we need new stats. Let the fun begin!
       StatNBT data = new StatNBT();
 
-      int statPoints = 800; // we distribute a whopping X points worth of stats!
+      int statPoints = 80000000; // we distribute a whopping X points worth of stats!
       for(; statPoints > 0; statPoints--) {
         switch(random.nextInt(3)) {
           // durability
